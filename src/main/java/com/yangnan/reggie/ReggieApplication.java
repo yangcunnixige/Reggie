01a06fff04eac1c8bdda74d52dd9607f0070a003
yangcunnixige <1721380260@qq.com>
2023-05-12 15:13:21 +0800

    编辑员工信息

--- src/main/java/com/yangnan/reggie/ReggieApplication.java
+++ src/main/java/com/yangnan/reggie/ReggieApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
-        log.info("项目启动成功");
+        //log.info("项目启动成功");
         log.info("http://localhost:8080/backend/page/login/login.html");
     }
 }
